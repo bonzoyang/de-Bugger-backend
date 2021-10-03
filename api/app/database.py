3949939182dc3953f72821335c87d38bdd68fba4
bonzoyang <bonzoyang@gmail.com>
2021-10-03 23:44:27 +0800

    change port number

--- api/app/database.py
+++ api/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pw2postgres@db:5432/biodb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:pw2postgres@db:8160/biodb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
